a77ec79a4739f81aa00a3fa3c5c58cf439ce8f74
Oleg Kumogorodskyy <98911703+Kuman13@users.noreply.github.com>
2023-08-26 22:40:05 +0200

    Add files via upload

---
+++ git/api_validation/first_code.js
@@ -0,0 +1,71 @@
+// letnewLet1 = 'ourValue'
+//Symbol
+// let symbolOne = Symbol()
+
+//String
+let stringOneBuKuLu = 'ORIGINAL TEXT'
+// let stringOneTwo = "jdjdjdjd"
+// let stringPhrase = `let says ${stringOneBuKuLu}`
+
+ console.log( typeof stringOneBuKuLue)
+
+//Muber
+let numberOne = 1
+
+console.log( stringOneBuKuLu / 0 )
+
+//Boolean
+let boolOne = true
+let boolTwo = false
+
+//NULL
+let nullOne = null
+console.log(nullOne)
+
+//undefined
+let undOne
+
+let undTwo = undefined
+
+console.log(undOne)
+
+
+
+var stringOneVar = 2323
+var ttt = 'Ofd,sfds'
+
+let numberOneOne = 39933
+let stringOOOO = 'dfkjkjgfg'
+
+const oneConstMy = 'username'
+
+
+let userNForE2eDenys = 'denys'
+let userN2 = 'denys'
+
+
+
+
+
+console.log(userNForE2eDenys);
+
+
+
+
+
+//We are good
+let message = "We are good";
+console.log(message)
+
+//Hello World
+let word1 = "Hello";
+let word2 = "World";
+console.log(word1 + " " + word2);
+
+//Value
+let myVariable = 'value';
+console.log(typeof myVariable);
+
+//Name
+let name = "John";
+console.log(`hello ${name}`);
---
+++ git/api_validation/type_conversion.js
@@ -0,0 +1,21 @@
+
+let boolOne = true
+let boolTwo = false
+
+let numberOne = Number (boolOne)
+
+console.log(numberOne)
+
+
+let numberStr = "239"
+
+let numberN = Number(numberStr)
+
+console.log(numberN)
+
+let numberN2 = 2032
+console.log( typeof numberN2)
+const numberINStone = String(numberN2)
+console.log(numberINStone)
+
+
